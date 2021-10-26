aa04a7ca47d3615a96de66aebed4211a5fa96c6b
Fabricio Winter <git@notfab.net>
2021-10-25 21:55:22 -0300

    Remove ByteBuddy hacks as it was fixed in 4f1d17a
    Signed-off-by: Fabricio Winter <git@notfab.net>

--- examples/jda/src/main/java/com/gelbpunkt/jda/Application.java
+++ examples/jda/src/main/java/com/gelbpunkt/jda/Application.java
@@ -1,36 +1,21 @@
 package com.gelbpunkt.jda;
 
 import lombok.extern.slf4j.Slf4j;
-import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.agent.ByteBuddyAgent;
-import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
-import net.bytebuddy.implementation.StubMethod;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
 import net.dv8tion.jda.api.utils.Compression;
-import net.dv8tion.jda.internal.handle.ReadyHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import javax.security.auth.login.LoginException;
 
-import static net.bytebuddy.matcher.ElementMatchers.named;
-
 @Slf4j
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        // Intercept handleReady and No-Op it because it's not needed since bots became a thing.
-        ByteBuddyAgent.install();
-        new ByteBuddy()
-                .redefine(ReadyHandler.class)
-                .method(named("handleReady"))
-                .intercept(StubMethod.INSTANCE)
-                .make()
-                .load(Application.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
         SpringApplication.run(Application.class, args);
     }
 
